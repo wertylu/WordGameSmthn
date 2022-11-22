a9a0222b8fbe4035136ea126d315ee83f40529e6
wertylu <alfeziat@gmail.com>
2022-11-22 18:58:50 +0200

    lab4

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-from typing import List
-from stuff.algos import find_max_chain
-from stuff.readnwrite import write_to_file
-from stuff.readnwrite import read_csv
+from collections import defaultdict
 
 
-def main(list: List[str]):
-    list.sort(key=lambda x: len(x))
-    dictionary = {}
-    return find_max_chain(list, dictionary)
+# Class to represent a graph
 
 
+class Graph:
+    def __init__(self, vertices):
+        self.graph = defaultdict(list)  # dictionary containing adjacency List
+        self.V = vertices  # No. of vertices
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    # A recursive function used by topologicalSort
+    def topologicalSortUtil(self, v, visited, stack):
+
+        # Mark the current node as visited.
+        visited[v] = True
+
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.topologicalSortUtil(i, visited, stack)
+
+        # Push current vertex to stack which stores result
+        stack.append(v)
+
+    # The function to do Topological Sort. It uses recursive
+    # topologicalSortUtil()
+    def topologicalSort(self):
+        # Mark all the vertices as not visited
+        visited = [False] * self.V
+        stack = []
+
+        # Call the recursive helper function to store Topological
+        # Sort starting from all vertices one by one
+        for i in range(self.V):
+            if visited[i] == False:
+                self.topologicalSortUtil(i, visited, stack)
+
+        # Print contents of the stack
+        print(stack[::-1])  # return list in reverse order
+
+
+# Driver Code
 if __name__ == '__main__':
-    file = 'input.csv'
-    write_to_file(main(read_csv(file)))
-    print(main(read_csv(file)))
+    g = Graph(6)
+    g.addEdge(5, 2)
+    g.addEdge(5, 0)
+    g.addEdge(4, 0)
+    g.addEdge(4, 1)
+    g.addEdge(2, 3)
+    g.addEdge(3, 1)
+
+    print("Following is a Topological Sort of the given graph")
+
+    # Function Call
+    g.topologicalSort()
